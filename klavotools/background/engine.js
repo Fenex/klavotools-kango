2ea7af019b039d4b8675260cbebda49b799ce941
Vitaliy <vitbusko@gmail.com>
2014-07-12 21:20:00 +0700

    icon's color depending on auth status

--- klavotools/background/engine.js
+++ klavotools/background/engine.js
@@ -45,10 +45,18 @@ KlavoTools.tabs = {
     }
 };
 
+KlavoTools.const = {
+    //PATH_TO_ICON: kango.io.getResourceUrl('res/skins/$1'),
+    ICON_DEFAULT: 'icons/digits.png',
+    ICON_AUTH: 'icons/normal.png',
+    ICON_UNREAD: 'icons/dic.png'
+};
+
 KlavoTools.Auth = {
     user: {
         id: null,
-        login: null
+        login: null,
+        unread: 0
     }, 
     get: function() {
         return KlavoTools.Auth.user;
@@ -57,11 +65,13 @@ KlavoTools.Auth = {
         var url = 'http://klavogonki.ru/api/profile/get-messages-contacts';
         var url_send = 'http://klavogonki.ru/api/profile/send-message';
         
-        function set(id, login) {
+        function set(id, login, unread) {
             console.log(id);
             
             KlavoTools.Auth.user.id = id;
             KlavoTools.Auth.user.login = login;
+            KlavoTools.Auth.user.unread = unread;
+            KlavoTools.Button.update();
             
             return null;
         }
@@ -69,7 +79,7 @@ KlavoTools.Auth = {
         function send() {
             kango.xhr.send({
                 method: 'POST',
-                url: 'http://klavogonki.ru/api/profile/send-message',
+                url: url_send,
                 headers:{
                     'Accept': 'application/json, text/plain, */*',
                     'Content-Type': 'application/json;charset=UTF-8'
@@ -89,22 +99,62 @@ KlavoTools.Auth = {
                 method: 'GET',
                 url: url
             }, function(res) {
+                if(!res.response)
+                    return set(null, null, 0);
                 var answer = JSON.parse(res.response);
-                if(answer.err) {
-                    return set(null, null);
-                }
+                if(answer.err)
+                    return set(null, null, 0);
                 
-                if(!answer.messages[0]) {
+                if(!answer.messages[0])
                     return send();
+
+                var unread = 0;
+                for(var i=0; i<answer.messages.length; i++) {
+                    if(answer.messages[i].folder == 'in')
+                        unread += answer.messages[i].unread;
                 }
                 
-                set(answer.messages[0].user_id);
+                set(answer.messages[0].user_id, null, unread);
             });
         }
         
         check();
-        setInterval(check, 1000 * 60);
+        setInterval(check, 1000 * 15);
     }
-}
+};
+
+KlavoTools.Button = {
+    setBadgeValue: function(num) {
+        if(num == 0)
+            num = '';
+        
+    },
+    update: function() {
+        var icon = 'ICON_DEFAULT';
+        var unread = '';
+        
+        if(!KlavoTools.Auth.user.id) {
+            KlavoTools.Auth.user.unread = 0;
+        } else {
+            if(KlavoTools.Auth.user.unread > 0) {
+                unread = KlavoTools.Auth.user.unread;
+                icon = 'ICON_UNREAD';
+            } else {
+                icon = 'ICON_AUTH';
+            }
+        }
+
+        kango.ui.browserButton.setIcon(KlavoTools.const[icon]);
+        kango.ui.browserButton.setBadgeValue(unread);
+    },
+    init: function() {
+        kango.ui.browserButton.setBadgeBackgroundColor([255, 0, 0, 255]);
+    }
+};
+
+KlavoTools.Mail = {
+    
+};
 
 KlavoTools.Auth.init();
+KlavoTools.Button.init();
