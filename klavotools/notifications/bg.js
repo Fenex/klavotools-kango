66b8d77958d523a9fcba709df109981d02c4813e
Vitaliy <vitbusko@gmail.com>
2014-07-18 00:15:10 +0700

    background part
    not completed yet

--- klavotools/notifications/bg.js
+++ klavotools/notifications/bg.js
@@ -1,3 +1,72 @@
 /**
-    Notification module
+    Notification module (background)
 */
+
+var NotificationList = function() {
+    this.list = {};
+};
+
+NotificationList.prototype = {
+    create: function(data) {
+        var id = this.getId();
+        this.list[id] = new Notification(
+            
+        );
+        return this.list[id];
+    },
+    getId: function() {
+        return new Date().getTime().toString(36);
+    },
+    count: function() {
+        var i=0;
+        for(var name in this.list)
+            i++;
+        
+        return i;
+    },
+    toString: function() {
+        return '[object NotificationList]';
+    }
+};
+
+var Notification = function(id, data) {
+    this.id = id;
+    
+    this.message = data.message;
+    this.title = data.title;
+    this.iconUrl = data.iconUrl;
+};
+
+Notification.prototype = {
+    show: function() {
+        var data = this.getBlank();
+        data.action = 'show';
+        dispatchToAllTabs(data);
+    },
+    hide: function() {
+        var data = this.getBlank();
+        data.action = 'hide';
+        dispatchToAllTabs(data);
+    },
+    getBlank: function() {
+        return {
+            id: this.id,
+            params: {
+                title: this.title,
+                message: this.message,
+                iconUrl: this.iconUrl
+            }
+        };
+    },
+    toString: function() {
+        return '[object Notification]';
+    }
+};
+
+var dispatchToAllTabs = function(data) {
+    kango.browser.tabs.getAll(function(tabs) {
+        for (var i=0; i<tabs.length; i++) {
+            tabs[i].dispatchMessage('Notification', data);
+        }
+    });
+};
