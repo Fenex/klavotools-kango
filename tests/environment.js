e3333a55b5ce2a83c99df347315789bc59e8f98c
Daniil <filippovdaniil@gmail.com>
2015-12-23 19:34:41 +0700

    (+) Added Q and chai-as-promised dependencies

--- tests/environment.js
+++ tests/environment.js
@@ -5,8 +5,10 @@
  */
 
 var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 chai.should();
 
 var kango = require('./kango-mock.js');
