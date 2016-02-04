354ee1d4d0326c956d9bad8c5d0d506b1fd3fdf0
Daniil <filippovdaniil@gmail.com>
2016-02-04 18:44:36 +0700

    (+) Working with ContextMenus #11
    Reworked foreground context-menu script, added context menu
    for vocabulary links.

--- klavotools/foreground/context-menu.js
+++ klavotools/foreground/context-menu.js
@@ -3,167 +3,144 @@
 // @include     http://klavogonki.ru/*
 // ==/UserScript==
 
-kango.invokeAsync('KlavoTools.Auth.get', function (auth) {
-    /**
-     * ContextMenu class constructor.
-     *
-     * @param {String} id An unique id for the <menu> element
-     */
-    function ContextMenu (id) {
-        this.id = id;
-        this.obj = document.createElement('menu');
-        this.obj.id = id;
-        this.obj.type = 'context';
-    }
+/**
+ * ContextMenu class constructor.
+ */
+function ContextMenu () {
+    this.obj = document.createElement('menu');
+}
 
-    /**
-     * Add an item to the menu.
-     *
-     * @param {String} label A text of the menu item
-     * @param {String} icon A path to the icon for the menu item
-     * @param {Function} callback An onclick callback function for the item
-     */
-    ContextMenu.prototype.addItem = function (label, icon, callback) {
-        var item = document.createElement('menuitem');
-        item.label = label;
-        item.onclick = callback;
+/**
+ * Adds an item to the menu.
+ *
+ * @param {Object} options An object with item parameters:
+ * @param {String} options.label An item's text
+ * @param {Function} options.callback An onclick callback function
+ * @param {String} [options.icon] A path to the icon for the menu's item
+ * @param {Boolean} [options.disabled=false] Wheither the item is disabled
+ */
+ContextMenu.prototype.addItem = function (options) {
+    var item = document.createElement('menuitem');
+    item.label = options.label;
+    item.onclick = options.callback;
+    if (options.icon) {
+        var icon = options.icon;
         kango.invokeAsync('kango.io.getResourceUrl', icon, function (url) {
             item.icon = url;
         });
-        this.obj.appendChild(item);
-    };
-
-    /**
-     * Set the data-* attribute for the menu element.
-     *
-     * @param {String} name Name of the attribute (without "data-")
-     * @param {String} value Value of the data attribute
-     */
-    ContextMenu.prototype.setData = function (name, value) {
-        this.obj.setAttribute('data-' + name, value);
-    };
-
-    /**
-     * Get the value of the data-* attribute for the menu element.
-     *
-     * @param {String} name Name of the attribute (without "data-")
-     * @return {String}
-     */
-    ContextMenu.prototype.getData = function (name) {
-        return this.obj.getAttribute('data-' + name);
-    };
-
-    /**
-     * Returns the <menu> html element.
-     *
-     * @return {Object}
-     */
-    ContextMenu.prototype.getElement = function () {
-        return this.obj;
-    };
-
-    var userContextMenu = new ContextMenu('klavotools_context_user');
-    // TODO: bring this to the background?
-    [
-        {
-            label: 'Открыть диалог',
-            icon: 'icons/context_messages.png',
-            url: '/u/#/%auth_id%/messages/%id%',
-        },
-        {
-            label: 'Сводка',
-            icon: 'icons/context_summary.png',
-            url: '/u/#/%id%',
-        },
-        {
-            label: 'Статистика',
-            icon: 'icons/context_statistics.png',
-            url: '/u/#/%id%/stats/',
-        },
-        {
-            label: 'Бортжурнал',
-            icon: 'icons/context_logbook.png',
-            url: '/u/#/%id%/journal/',
-        },
-        {
-            label: 'Достижения',
-            icon: 'icons/context_achievements.png',
-            url: '/u/#/%id%/achievements/',
-        },
-        {
-            label: 'Друзья',
-            icon: 'icons/context_friends.png',
-            url: '/u/#/%id%/friends/',
-        },
-        {
-            label: 'Гараж',
-            icon: 'icons/context_garage.png',
-            url: '/u/#/%id%/car/',
-        },
-    ].forEach(function (item) {
-        if (!auth.id && item.url.indexOf('%auth_id%') + 1) {
-            return;
-        }
-        userContextMenu.addItem(item.label, item.icon, function () {
-            var path = makeURL(this.url, this.menu.getData('id'));
-            window.location.href = window.location.origin + path;
-        }.bind({ menu: userContextMenu, url: item.url }));
-    });
-    document.body.appendChild(userContextMenu.getElement());
-
-
-    /**
-     * Substitutes variables into the URL's path
-     *
-     * @param {String} template The URL's path template
-     * @param {String} [id] The value of the %id% variable
-     * @return {String}
-     */
-    function makeURL (template, id) {
-        if (id) {
-            template = template.replace(/%id%/, id);
-        }
-        if (auth.id) {
-            template = template.replace(/%auth_id%/, auth.id);
-        }
-        return template;
     }
+    if (options.disabled) {
+        item.disabled = 'disabled';
+    }
+    this.getElement().appendChild(item);
+};
 
-    /**
-     * For the given <a> element, checks it's "href" attribute for the id of
-     * user or vocabulary, and in case of success, adds the "contextmenu"
-     * attribute with appropriate value (id of the <menu> element).
-     *
-     * @param {Object} link
-     */
-    function updateLink (link) {
-        // A RegExp for getting the user's or vocabulary's id from the link's
-        // href:
-        var re = /(profile|u\/#|vocs)\/(\d+)/;
-        var matches = link.href.match(re);
-        if (!matches) {
-            return;
-        }
+/**
+ * Adds a submenu
+ *
+ * @param {String} label A submenu's label
+ * @param {Object} menu An instance of the ContextMenu class
+ */
+ContextMenu.prototype.addSubmenu = function (label, menu) {
+    var menuElement = menu.getElement();
+    menuElement.label = label;
+    this.getElement().appendChild(menuElement);
+};
 
-        var menuId = userContextMenu.id;
-        if (matches[1] === 'vocs') {
-            // TODO:
-            //menuId = 'klavotools_context_voc';
-        } else {
-            userContextMenu.setData('id', matches[2]);
+/**
+ * Returns the <menu> html element.
+ *
+ * @return {Object}
+ */
+ContextMenu.prototype.getElement = function () {
+    return this.obj;
+};
+
+function createMenu (menu) {
+    var container = new ContextMenu;
+    menu.items.forEach(function (item) {
+        // Recursively adding submenus:
+        if (item.items instanceof Array) {
+            var submenu = createMenu(item);
+            return container.addSubmenu(item.label, submenu);
         }
+        var callback = function () {};
+        if (item.url) {
+            // TODO: use promises:
+            callback = function () {
+                kango.invokeAsync('KlavoTools.ContextMenus.makeRedirectURL',
+                  this.url,
+                  function (path) {
+                    window.location.href = window.location.origin + path;
+                });
+            }.bind({ url: item.url });
+        }
+        container.addItem({
+            label: item.label,
+            icon: item.icon,
+            disabled: item.disabled,
+            callback: callback,
+        });
+    });
+    return container;
+}
+
+function setupMenu (id, structure) {
+    var menu = createMenu(structure).getElement();
+    menu.id = id;
+    menu.type = 'context';
+    document.body.appendChild(menu);
+    return menu;
+}
+
+var userContextMenu;
+var vocContextMenu;
+
+kango.invokeAsync('KlavoTools.ContextMenus.getUserMenuStructure',
+  function (structure) {
+    userContextMenu = setupMenu('klavotools_context_user', structure);
+});
+kango.invokeAsync('KlavoTools.ContextMenus.getVocMenuStructure',
+  function (structure) {
+    vocContextMenu = setupMenu('klavotools_context_vocabulary', structure);
+});
+
+
+// Black magic: copying the parseLinkURL static method from the background
+// script, with a view to the possibility of setting the "contextmenu"
+// attribute at the same time, when the oncontextmenu event occurs.
+var parseLinkURL;
+kango.invokeAsync('ContextMenus.parseLinkURL.toString', function (funcBody) {
+    eval('parseLinkURL = ' + funcBody);
+});
+
+/**
+ * "Binds" the given <a> element to corresponding context menu.
+ *
+ * @param {Object} link
+ */
+function bindLink (link) {
+    var params = parseLinkURL(link.href);
+    var menuId;
+    if (params.user_id ) {
+        menuId = userContextMenu.id;
+    } else if (params.voc_id) {
+        menuId = vocContextMenu.id;
+    }
+    if (menuId) {
         link.setAttribute('contextmenu', menuId);
     }
+    kango.invokeAsync('KlavoTools.ContextMenus.setParams', params);
+}
 
-    document.addEventListener('contextmenu', function (event) {
-        var current = event.target;
-        while (current.parentNode) {
-            if (current.tagName === 'A') {
-                updateLink(current);
-                break;
-            } else {
-                current = current.parentNode;
-            }
+document.addEventListener('contextmenu', function (event) {
+    var current = event.target;
+    while (current.parentNode) {
+        if (current.tagName === 'A') {
+            bindLink(current);
+            break;
+        } else {
+            current = current.parentNode;
         }
-    });
+    }
 });
