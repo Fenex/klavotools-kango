a3cad8321a44db5dbe0cd5b578695255be801c1b
Daniil <filippovdaniil@gmail.com>
2016-07-30 15:35:09 +0700

    (+) Added CommonJS modules support for tests/loader.js

--- tests/loader.js
+++ tests/loader.js
@@ -1,5 +1,6 @@
 /**
- * @file A simple helper for loading "vanilla" JavaScript modules
+ * @file A simple helper for loading "vanilla" and CommonJS JavaScript modules
+ * to the global scope.
  * @author Daniil Filippov <filippovdaniil@gmail.com>
  */
 
@@ -11,9 +12,63 @@ var path = require('path');
 global.kango = require('./kango-mock.js');
 
 /**
- * @param {String} modulePath A path to the "vanilla" JavaScript module
+ * @param {string} modulePath A path to the "vanilla" JavaScript module
+ * @returns {Object} A reference to the ModuleLoader function object.
  */
-module.exports = function (modulePath) {
+function ModuleLoader (modulePath) {
+  ModuleLoader.loadDependency({ path: modulePath });
+  return ModuleLoader;
+}
+
+/**
+ * Loads a module to the global scope.
+ * @param {(string|Object)} options A path to the "vanilla" JavaScript module, or
+ *  an object with module parameters.
+ * @param {string} options.path A path to the module.
+ * @param {string} options.globalName A global name for dependency (used only for
+ *  CommonJS modules).
+ * @param {('vanilla'|'commonjs')} options.type A module type.
+ * @returns {Object} A reference to the ModuleLoader function object.
+ * @static
+ */
+ModuleLoader.loadDependency = function (options) {
+  if (typeof options === 'string') {
+    options = { path: options };
+  } else if (!options || typeof options !== 'object') {
+    var msg = 'Argument should be either a string or an object';
+    throw new TypeError(msg);
+  }
+
+  if (!options.type || options.type === 'vanilla') {
+    return ModuleLoader.loadVanillaModule(options.path);
+  } else if (options.type === 'commonjs') {
+    return ModuleLoader.loadCommonJSModule(options.globalName, options.path);
+  }
+};
+
+/**
+ * Loads a "vanilla" JavaScript module to the global scope.
+ * @param {string} modulePath A path to the module.
+ * @returns {Object} A reference to the ModuleLoader function object.
+ * @static
+ */
+ModuleLoader.loadVanillaModule = function (modulePath) {
   var data = fs.readFileSync(path.normalize(modulePath));
   vm.runInThisContext(data);
+  return ModuleLoader;
 };
+
+/**
+ * Loads a CommonJS module to the global scope.
+ * @param {string} globalName A global variable name for the dependency.
+ * @param {string} modulePath A path to the module.
+ * @returns {Object} A reference to the ModuleLoader function object.
+ * @static
+ */
+ModuleLoader.loadCommonJSModule = function (globalName, modulePath) {
+  global[globalName] = require(path.resolve('.', modulePath));
+  return ModuleLoader;
+};
+
+
+module.exports = ModuleLoader;
