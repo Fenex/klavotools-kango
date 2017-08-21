8a1ecd696e3ec833fa41be887df5bd2197a2fe91
Daniil <filippovdaniil@gmail.com>
2017-08-21 10:55:41 +0700

    (+) WebExtension building

--- dodo.py
+++ dodo.py
@@ -4,9 +4,11 @@ from zipfile import ZipFile
 from tempfile import mkstemp, mkdtemp
 from subprocess import Popen, PIPE
 from shutil import rmtree, copytree, move, ignore_patterns
-from os import path, close, remove, listdir, makedirs
+from os import path, close, remove, listdir, walk, makedirs
 import sass
+import json
 
+WEBEXTENSION_ID = 'klavotools-kango@klavogonki.ru'
 KANGO_ARCHIVE_URL = 'http://kangoextensions.com/kango/kango-framework-latest.zip'
 KANGO_DIR = 'kango'
 KANGO_BIN = path.abspath(path.join(KANGO_DIR, 'kango.py'))
@@ -45,6 +47,21 @@ def buildProject(kango, targetDir):
     pipe = Popen(['python', kango, 'build', '.'], cwd=targetDir)
     pipe.communicate(None)
 
+def buildWebExtension(srcDir, destDir):
+    with open(path.join(srcDir, 'manifest.json')) as data:
+        manifest = json.load(data)
+    name = manifest['name'].lower().replace(' ', '') + '_we_' + manifest['version'] + '.xpi'
+    manifest['applications'] = { 'gecko': { 'id': WEBEXTENSION_ID } }
+    with open(path.join(srcDir, 'manifest.json'), 'w') as outfile:
+        json.dump(manifest, outfile, indent=2)
+    createDirIfNotExists(destDir)
+    archive = ZipFile(path.join(destDir, name), 'w')
+    for root, dirs, files in walk(srcDir):
+        for fileName in files:
+            archive.write(path.join(root, fileName), path.join(path.relpath(root, srcDir), fileName))
+    archive.close()
+
+
 def createDirIfNotExists(targetDir):
     if not path.exists(targetDir):
         makedirs(targetDir)
@@ -58,6 +75,9 @@ def moveFiles(srcDir, destDir):
     createDirIfNotExists(destDir)
     for fileName in files:
         pathName = path.join(srcDir, fileName)
+        if fileName == 'chrome':
+            buildWebExtension(pathName, destDir)
+            continue
         if path.isfile(pathName):
             move(pathName, path.join(destDir, fileName))
 
