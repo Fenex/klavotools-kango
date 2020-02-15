98dfc104d5074b7baea12aa9e7ffdaf63c076944
Vitaliy Busko <vitaliy.opensource@gmail.com>
2020-02-15 15:28:19 +0700

    Created angular-service that can be used to get correct protocol

---
+++ klavotools/foreground/lib/protocol.js
@@ -0,0 +1,29 @@
+angular.module('klavotools.protocol', [])
+
+.factory('Protocol', function ($q) {
+    return {
+        convert: function (url) {
+            var defer = $q.defer()
+            chrome.runtime.sendMessage(
+                {
+                    name: 'protocol/convert',
+                    url: url
+                },
+                function (res) { defer.resolve(res.url) }
+            )
+
+            return defer.promise
+        }
+    };
+})
+
+.directive('kgPath', function (Protocol) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            Protocol.convert(attrs.href).then(function (url) {
+                element.attr('href', url)
+            })
+        }
+    }
+})
