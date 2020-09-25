4630419a6aa2cf4adf963703a00a5800243225f5
Vitaliy Busko <vitaliy.opensource@gmail.com>
2020-09-25 23:51:47 +0700

    syntax err, version bump (3.6.3)

--- klavotools/foreground/protocol.js
+++ klavotools/foreground/protocol.js
@@ -1,14 +1,13 @@
 if (location.host === 'klavogonki.ru') {
     // do not modify location if we on remind page with `key` param (reset pwd process)
     // see https://github.com/Fenex/klavotools-kango/issues/45
-    if (/\/remind.*[\?&]key=[\da-fA-F]{32}/.test(location.href))
-        return;
-
-    chrome.runtime.sendMessage({
-        name: 'protocol/opened-tab',
-        url: location.href
-    }, function (response) {
-        if (response.redirect)
-            location.href = response.redirect
-    })
+    if (!/\/remind.*[\?&]key=[\da-fA-F]{32}/.test(location.href)) {
+        chrome.runtime.sendMessage({
+            name: 'protocol/opened-tab',
+            url: location.href
+        }, function (response) {
+            if (response.redirect)
+                location.href = response.redirect
+        })
+    }
 }
